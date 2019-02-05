fc73e66bd55f9f0822fcfae2b13e3c4f5ce34057
Felipe Silva <felipeferreirasilva@gmail.com>
2019-02-06 12:53:10 +1300

    Add Posts Routes

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 const bodyParser = require('body-parser')
 const errorHandler = require('./handlers/err')
 const authRoutes = require('./routes/auth')
+const postsRoutes = require('./routes/posts')
 
 const PORT = 8081
 
@@ -12,6 +13,7 @@ app.use(cors())
 app.use(bodyParser.json())
 
 app.use('/api/auth', authRoutes)
+app.use('/api/users/:id/posts', postsRoutes)
 
 app.use(function(req, res, next){
     let err = new Error('Not Found')
---
+++ server/routes/posts.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const router = express.Router({mergeParams: true})
+
+const { createPost } = require('../handlers/posts')
+
+router.route('/').post(createPost)
